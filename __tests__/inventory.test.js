c9cb8daa5c29fb338a68599a0681fb1b12aa602f
Jesse White <jesse.white6@gmail.com>
2021-04-14 15:49:02 -0700

    add passing tests for acquireItem method. Co-authored-by: Noah Woodrum <heymymomthinksimcool@gmail.com> Co-authored-by: Patrick Lee <patricklee1138@gmail.com>

--- __tests__/inventory.test.js
+++ __tests__/inventory.test.js
@@ -1,12 +1,57 @@
 import Inventory from './../src/js/inventory.js'
 
 describe('Inventory', () => {
-    test("it should create a new inventory object with the users input.", () => {
-        const newInventory = new Inventory("swim", "leather wraps", "broadsword", "potion");
-        expect(newInventory.skills).toEqual("swim");
-        expect(newInventory.armor).toEqual("leather wraps");
-        expect(newInventory.weapons).toEqual("broadsword");
-        expect(newInventory.consumables).toEqual("potion");
-
+    test("it should create a new inventory object with all skills set to false.", () => {
+        const newInventory = new Inventory();
+        expect(newInventory.skills.swim).toEqual(false);
+        expect(newInventory.skills.climb).toEqual(false);
+    });
+    // test("it should create a new inventory object with the users input.", () => {
+    // const newInventory = new Inventory("swim", "leather wraps", "broadsword", "potion");
+    // expect(newInventory.skills).toEqual("swim");
+    // expect(newInventory.armor).toEqual("leather wraps");
+    // expect(newInventory.weapons).toEqual("broadsword");
+    // expect(newInventory.consumables).toEqual("potion");
+    // });
+    test("it should enable a skill", () => {
+        const newInventory = new Inventory();
+        expect(newInventory.enableSkill("swim"));
+        expect(newInventory.skills.swim).toEqual(true);
+        expect(newInventory.skills.climb).toEqual(false);
+    });
+    test("It should disable a skill", () => {
+        const newInventory = new Inventory();
+        expect(newInventory.enableSkill("swim"));
+        expect(newInventory.disableSkill("swim"));
+        expect(newInventory.skills.swim).toEqual(false);
+    });
+    test("it should check to see what the value of the skill is before changing it.", () => {
+        const newInventory = new Inventory();
+        expect(newInventory.toggleSkill("swim"));
+        expect(newInventory.skills.swim).toEqual(true);
+    });
+    test("it should check to see what the value of the skill is before changing it.", () => {
+        const newInventory = new Inventory();
+        expect(newInventory.toggleSkill("swim"));
+        expect(newInventory.toggleSkill("swim"));
+        expect(newInventory.skills.swim).toEqual(false);
+    });
+    test("it should push an armor type into the armorArray", () => {
+        const newInventory = new Inventory();
+        const armorItem = { type: "armor" };
+        expect(newInventory.acquireItem(armorItem));
+        expect((newInventory.armor).includes(armorItem)).toEqual(true);
+    });
+    test("it should push a weapon type to the weaponArray", () => {
+        const newInventory = new Inventory();
+        const weaponItem = { type: "weapon" };
+        expect(newInventory.acquireItem(weaponItem));
+        expect((newInventory.weapon).includes(weaponItem)).toEqual(true);
+    });
+    test("it should push a consumable type to the consumablesArray", () => {
+        const newInventory = new Inventory();
+        const consumableItem = { type: "consumables" };
+        expect(newInventory.acquireItem(consumableItem));
+        expect((newInventory.consumables).includes(consumableItem)).toEqual(true);
     });
 });
--- src/js/inventory.js
+++ src/js/inventory.js
@@ -1,12 +1,75 @@
 export default class Inventory {
     constructor(skills, armor, weapons, consumables) {
-        this.skills = { skills };
-        this.armor = armor;
-        this.weapons = weapons;
-        this.consumables = consumables;
+        this.skills = {
+            swim: false,
+            climb: false,
+            stealth: false,
+            steal: false,
+            "dual wield": false,
+            fly: false,
+            intimidate: false,
+            diplomacy: false,
+            acrobatics: false,
+            perception: false,
+            heal: false
+        };
+        this.armor = [];
+        this.weapons = [];
+        this.consumables = [];
     }
-} fgyedasogfuyesf
+    enableSkill(skillName) {
+        this.skills[skillName] = true;
+    }
+    disableSkill(skillName) {
+        this.skills[skillName] = false;
+    }
+    toggleSkill(skillName) {
+        if (this.skills[skillName] === true) {
+            this.skills[skillName] = false;
+        } else {
+            this.skills[skillName] = true;
+        };
+    };
+    acquireItem(item) {
+        let armorArray = this.armor; // sets "armorArray" to a property on the character
+        let weaponArray = this.weapons;
+        let consumablesArray = this.consumables;
+        if (item.type === "armor") {
+            armorArray.push(item);
+        } else if (item.type === "weapon") {
+            weaponArray.push(item);
+        } else {
+            consumablesArray.push(item);
+        }
+        this.armor = armorArray;
+        this.weapon = weaponArray;
+        this.consumables = consumablesArray;
+    };
+};
+
+//chestPlate {
+//type: armor
+//class: warrior
+//dual-wield: false
+//value: 5
+// }
+
+//playerInventory(consumables[], ammunition[], )
+//playerEqupment(armorSet[],weaponSlots[], ringSlots[])
 
 // this.skills = {an empty object that will take string values and set them to either true or false depending on item.}
 // every key value within Inventory will be another oject with preset key values that we can change to either true or false whether or not we have that item/ability.
 // key values will be arrays that can be accessed once a player obtains that stat/item
+// e.g., check if player has skill "swim" -- yes: can cross water, no: water is a boundary
+//
+// What's in a skill?
+// Just a string, which acts as a keyword
+// Since we can have multiple skills (swim, climb, stealth, etc.) they should be true/false properties in the skills object: if (character.Inventory.skills.swim === true) { //do stuff };
+// skills {
+    // swim: true,
+    // climb: false,
+    // stealth: false
+// }
+//armor will be an array of items that can be removed and added as items are used and replaced.
+//only certain classes of characters can utilze certain armor types.
+//e.g. universal armor = everyone, mage armor = only mages.
