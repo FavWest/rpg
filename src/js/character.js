7fae0f97c71f92ee25e4644acef5a3878f03b83d
Tori FavWest <practiceandthenpractice@gmail.com>
2021-04-14 10:53:58 -0700

    pass test: Character constructor works as expected
    Co-authored-by: Jesse White  <jesse.white6@gmail.com>
    Co-authored-by: Michael Burton <micbur1@gmail.com>
    Co-authored-by: Maxwell Meyer <maxreadswell@gmail.com>
    Co-authored-by: Noah Woodrum <heymymomthinksimcool@gmail.com>
    Co-authored-by: Grafton Borge  <graftonmborge@gmail.com>

--- src/js/character.js
+++ src/js/character.js
@@ -6,9 +6,7 @@ export default class Character {
     this.physical=physical;
     this.mental=mental;
     this.hp=hp;
-    this.inventory={};
-
-  //let myFighter = new Character("Trog", "Warrior", 1, 3, 2, 10, {skills: ["climb"], weapons: ["plain sword"]}); //(name, class, level, physical, mental, hp, inventory)
-}
+    this.inventory=inventory;
+  }
 }
 
